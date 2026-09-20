a42708789625d06ec971f5c88e9dfe99fc0fdd01
hb248 <mt231029@fhstp.ac.at>
2026-09-20 10:54:24 +0200

    created todolist

--- ue1/src/components/TodoList.vue
+++ ue1/src/components/TodoList.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import type { Todo } from '../types';
+
+
+defineProps<{
+    todos: Array<Todo>;
+}>();
+
+const emit = defineEmits<{
+    (e: 'toggleTodo', id: number): void;
+    (e: 'deleteTodo', id: number): void;
+}>();
 
 </script>
 
 <template>
     <div>
         <h1>Todo List</h1>
+        <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @toggleTodo="emit('toggleTodo', todo.id)" @deleteTodo="emit('deleteTodo', todo.id)" />
     </div>
 </template>
